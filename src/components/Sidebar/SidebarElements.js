08524204a656661989628fd9aa3448986cf8d899
Germán Bonacchi <bonacchigerman@gmail.com>
2022-07-04 10:10:32 -0300

    add custom sideBar

--- src/components/Sidebar/SidebarElements.js
+++ src/components/Sidebar/SidebarElements.js
@@ -7,7 +7,7 @@ export const SidebarContainer = styled.aside`
  z-index: 999;
  width: 100%;
  height: 100%;
- background: #0d0d0d;
+ background: #010606;
  display: grid;
  align-items: center;
  top: 0;
