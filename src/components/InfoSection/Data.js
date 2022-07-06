1f3bdc327db961cc90072c76915c34d1516e14a0
Your Name <bonacchigerman@gmail.com>
2022-07-06 15:53:09 -0300

    fix translations inglish

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -1,91 +1,91 @@
-import svg1 from '../../images/svg-1.svg'
+import svg1 from "../../images/svg-1.svg";
 
 export const homeObjAbout = {
- id: 'about',
- lightBg: false,
- lightHeading: true,
- lightDesc: true,
- topLine: 'About Me',
- description:
-  'I am leading a team with the objective of analyzing and solving known problems throughout LATAM developing innovative solutions and giving VTEX IO trainings.',
- imgStart: false,
- img: svg1,
- alt: 'developer',
- buttonLabel: '',
- primary: true,
- dark: true,
- heading1: 'My name is Germán and I am an ',
- heading2: 'Apps Engineer',
- heading3: ' of the ',
- heading4: 'VTEX',
- heading5: ' Regional Product Team LATAM',
-}
+  id: "about",
+  lightBg: false,
+  lightHeading: true,
+  lightDesc: true,
+  topLine: "About Me",
+  description:
+    "I am leading a team whose goal is solving known problems within LATAM by developing innovative solutions and giving VTEX IO trainings.",
+  imgStart: false,
+  img: svg1,
+  alt: "developer",
+  buttonLabel: "",
+  primary: true,
+  dark: true,
+  heading1: "My name is Germán and I am an ",
+  heading2: "Apps Engineer",
+  heading3: " of the ",
+  heading4: "VTEX",
+  heading5: " Regional Product Team LATAM",
+};
 
 export const homeObjSkills = {
- id: 'skills',
- lightBg: true,
- lightHeading: false,
- lightDesc: false,
- topLine: 'My Skills',
- heading: 'At vero eos et accusamus et iusto odio dignissimos ducimus',
- description:
-  'Since I started my path as a developer almost 5 years ago, I have worked and learned many technologies and methodologies.',
- description2:
-  'Starting with Visual Basic in my first job at IPLUSB S.A, then at ALTOCOM S.R.L. I work as a co-analyst, developer, tester... all in one. So I had to internalize new knowledge like Oracle APEX, Oracle Database and PL/SQL, and when all of this was not enough to accomplish the client needs, I began to learn JavaScript by trial and error on my own.',
- description3:
-  'Currently at VTEX I had the biggest challenge so far since I had to learn TypeScript, React with Hooks, GraphQL and NodeJS to be able to develop applications and support customers and partners. As a leader I had to improve my team management by implementing Scrum as an agile methodology and defining workflows with the other teams we support.',
- description4:
-  'Parallel to my entire career, I have been studying since 2016 at the Universidad Nacional de Avellaneda to be a Software Engineer.',
- imgStart: true,
- skills: [
-  'C++',
-  'Java',
-  'Visual Basic',
-  'Oracle Database',
-  'Oracle APEX',
-  'PL/SQL',
-  'JavaScript',
-  'TypeScript',
-  'React with Hooks',
-  'GraphQL',
-  'NodeJS',
-  'git',
-  'Scrum',
- ],
- buttonLabel: '',
- primary: false,
- dark: false,
-}
+  id: "skills",
+  lightBg: true,
+  lightHeading: false,
+  lightDesc: false,
+  topLine: "My Skills",
+  heading: "",
+  description:
+    "Since I started developing, almost 5 years ago, I have worked and learned many technologies and methodologies, starting with Visual Basic at IPLUSB S.A, my first job.",
+  description2:
+    "Then as a co-analyst, developer and tester at ALTOCOM S.R.L. I have worked with Oracle APEX, Oracle Database and PL/SQL. At last, but not least, i learned JavaScript by my own in order to satisfy the customer needs.",
+  description3:
+    "Currently at VTEX I have had the biggest challenge so far, learning TypeScript, React with Hooks, GraphQL and NodeJS to be able to develop applications and give support  to customers and partners. As a leader I had to improve my team management by implementing Scrum as an agile methodology and defining workflows with the other teams we support.",
+  description4:
+    "At the same, I have been studying since 2016 at Universidad Nacional de Avellaneda to be a Software Engineer.",
+  imgStart: true,
+  skills: [
+    "C++",
+    "Java",
+    "Visual Basic",
+    "Oracle Database",
+    "Oracle APEX",
+    "PL/SQL",
+    "JavaScript",
+    "TypeScript",
+    "React with Hooks",
+    "GraphQL",
+    "NodeJS",
+    "git",
+    "Scrum",
+  ],
+  buttonLabel: "",
+  primary: false,
+  dark: false,
+};
 
 export const homeObjTraining = {
- id: 'trainings',
- lightBg: false,
- lightHeading: true,
- lightDesc: true,
- topLine: 'Trainings',
- heading:
-  'Here you can view the trainings I did... and soon there will be more',
- description:
-  'Giving training is one of the things I enjoy the most and where I can internalize knowledge by helping others to be better in the process.',
- imgStart: false,
- alt: '',
- buttonLabel: '',
- primary: true,
- dark: true,
-}
+  id: "trainings",
+  lightBg: false,
+  lightHeading: true,
+  lightDesc: true,
+  topLine: "Trainings",
+  heading:
+    "Here you can view the trainings I did... and soon there will be more",
+  description:
+    "Giving training is one of the things I enjoy the most and where I can internalize knowledge by helping others to be better in the process.",
+  imgStart: false,
+  alt: "",
+  buttonLabel: "",
+  primary: true,
+  dark: true,
+};
 
 export const homeObjContact = {
- id: 'contact',
- lightBg: true,
- lightHeading: false,
- lightDesc: false,
- topLine: 'Contact',
- heading: '',
- description: '',
- imgStart: true,
- img: '',
- alt: 'developer',
- buttonLabel: '',
- primary: true,
- dark: true,
-}
+  id: "contact",
+  lightBg: true,
+  lightHeading: false,
+  lightDesc: false,
+  topLine: "Contact",
+  heading: "",
+  description: "",
+  imgStart: true,
+  img: "",
+  alt: "developer",
+  buttonLabel: "",
+  primary: true,
+  dark: true,
+};
