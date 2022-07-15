c64fd1a8344031edaf7da0260b31ee29f4c43855
Your Name <bonacchigerman@gmail.com>
2022-07-15 09:12:23 -0300

    remove instagram

--- src/components/SocialIcons/SocialIconsElements.js
+++ src/components/SocialIcons/SocialIconsElements.js
@@ -7,7 +7,8 @@ export const SocialIconsContainer = styled.div`
   align-items: center;
   margin-top: ${({ component }) => (component === "SideBar" ? "1.2rem" : "")};
   margin-left: ${({ component }) => (component === "NavBar" ? "20px" : "")};
-  width: ${({ component }) => (component === "Footer" ? "240px" : "120px")};
+  //width: ${({ component }) => (component === "Footer" ? "240px" : "120px")};
+  width: ${({ component }) => (component === "Footer" ? "120px" : "80px")};
   justify-self: ${({ component }) => (component === "SideBar" ? "center" : "")};
 `;
 export const SocialIconLink = styled.a`
